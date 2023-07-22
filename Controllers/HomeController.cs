b660a5633d81abbe506c62a620b6c03083145a9b
Văn Thế <vvthe8102002@gmail.com>
2023-07-22 15:32:01 +0700

    Giao diện Job

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -54,9 +54,6 @@ namespace JobExchange.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-        public IActionResult Job()
-        {
-            return View();
-        }
+       
     }
 }
