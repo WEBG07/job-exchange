afc4789111bea3996b3058ebdd8a0622613312fa
mannv291201 <88828150+mannv291201@users.noreply.github.com>
2023-07-23 17:51:19 +0700

    SaveRecruitment

--- Controllers/JobController.cs
+++ Controllers/JobController.cs
@@ -1,15 +1,25 @@
-﻿using JobExchange.Models;
+﻿using JobExchange.Areas.Identity.Data;
+using JobExchange.Models;
+using JobExchange.Repository;
 using JobExchange.Repository.RepositoryInterfaces;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace JobExchange.Controllers
 {
     public class JobController : Controller
     {
         private readonly IRecruitmentRepository _recruitmentRepository;
-        public JobController(IRecruitmentRepository recruitmentRepository)
+        private readonly ISaveJobRepository _saveJobRepository;
+        private readonly UserManager<JobExchangeUser> _userManager;
+        private readonly JobExchangeContext _context;
+        public JobController(IRecruitmentRepository recruitmentRepository, JobExchangeContext context, ISaveJobRepository saveJobRepository, UserManager<JobExchangeUser> userManager)
         {
             _recruitmentRepository = recruitmentRepository;
+            _saveJobRepository = saveJobRepository;
+            _userManager = userManager;
+            _context = context;
         }
         public IActionResult Index()
         {
@@ -26,14 +36,64 @@ namespace JobExchange.Controllers
                 RecruitmentsCompanyId = recruitmentsByCompanyId,
                 RecruitmentsIndustryId = recruitmentsByIndustryId
             };
+            bool isSave = _saveJobRepository.ExistsById(_userManager.GetUserId(User), id);
+
+            ViewBag.IsSave = isSave; // Pass the result to the view
             return View(recruitmentViewModel);
         }
-        public IActionResult CandidateHistory() { 
-            return View();
-        }
-        public IActionResult SavedJobs()
+        public IActionResult CandidateHistory()
         {
             return View();
+        }
+
+        public IActionResult Saved()
+        {
+            var saved = _context.SaveRecruitments
+                .Include(r => r.Recruitment)
+                .ThenInclude(r => r.Company)
+                .Where(m => m.CandidateId == _userManager.GetUserId(User)).ToList(); // 1 list
+            //.FirstOrDefault(m => m.CandidateId == _userManager.GetUserId(User)); 1 đối tượng
+            return View(saved);
+
+        }
+
+        [HttpPost]
+        public IActionResult Save(string recruitmentId)
+        {
+            try
+            {
+                SaveRecruitment saveRecruitment = new SaveRecruitment();
+                saveRecruitment.RecruitmentId = recruitmentId;
+                saveRecruitment.CreateDate = DateTime.Now;
+                saveRecruitment.CandidateId = _userManager.GetUserId(User);
+                _saveJobRepository.Save(saveRecruitment);
+                return Ok(saveRecruitment);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPost]
+        public IActionResult UnSave(string Id)
+        {
+            try
+            {
+                var candidateId = _userManager.GetUserId(User);
+                var saveRecruitment = _saveJobRepository.GetById(candidateId, Id);
+                if (saveRecruitment == null)
+                {
+                    return NotFound();
+                }
+
+                _saveJobRepository.UnSave(saveRecruitment);
+                return Ok();
+            }
+            catch
+            {
+                return BadRequest();
+            }
         }
     }
 }
---
+++ wwwroot/js/savejob.js
@@ -0,0 +1,132 @@
+﻿
+function saveJob(id) {
+
+    var saveJobData = {
+        recruitmentId: id
+    };
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', '/Job/Save?recruitmentId=' + id, true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            /*toastr.success("Lưu tin thành công!");*/
+            location.reload();
+        }
+    };
+    xhr.send(saveJobData);
+}
+
+function unSave(id) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", '/Job/UnSave/' + id);
+    // Set the request headers
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onload = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            /*toastr.success("Bỏ lưu thành công")*/
+            location.reload();
+        } else {
+            toastr.error("Mã lỗi: " + xhr.status);
+        }
+    };
+    xhr.onerror = function () {
+        toastr.error("Đã xảy ra lỗi.");
+    };
+
+    xhr.send();
+}
+
+//    // Show a confirmation dialog
+//    var confirmDelete = confirm("Bạn có chắc muốn xóa ngành nghề này?");
+//    if (confirmDelete) {
+//        xhr.send();
+//    }
+//}
+
+
+//function resetText() {
+//    document.getElementById("IndustryName").value = "";
+//    document.getElementById("message").innerHTML = "";
+//    document.getElementById("IndustryImage").value = "";
+//}
+
+//function getIndustryById(id) {
+//    var IndustryName = document.getElementById("IndustryName");
+
+//    var request = new XMLHttpRequest();
+//    request.onreadystatechange = function () {
+//        if (this.readyState == 4 && this.status == 200) {
+//            var res = JSON.parse(this.responseText);
+//            idUpdate = res.industryId;
+//            IndustryName.value = res.industryName;
+//            showModal();
+//        }
+//    };
+//    request.open("GET", `/Industry/GetById/${id}`, true);
+//    request.send();
+
+//}
+
+//function createOrUpdate() {
+//    if (idUpdate == 0) {
+//        createIndustry();
+//    } else {
+//        updateIndustry();
+//    }
+//}
+
+//function searchIndustries() {
+//    var name = document.getElementById("name").value;
+//    console.log("Name: " + name);
+//    var xhr = new XMLHttpRequest();
+//    xhr.onreadystatechange = function () {
+//        if (this.readyState == 4 && this.status == 200) {
+//            var industries = JSON.parse(xhr.responseText);
+//            var html = "";
+//            for (var i = 0; i < industries.length; i++) {
+//                var industry = industries[i];
+//                var STT = 1;
+//                STT = i + 1;
+//                html += `<tr id="` + industry.industryId + `">
+//                    <td class="col-1">` + STT + `</td>
+//                    <td id="industry-@item.IndustryId" class="col-5">`+ industry.industryName + `</td>
+//                     <td class="col-3">
+//                         <img class="rounded" style="width: 250px;" src="/images/industry/`+ industry.industryImage + `" alt="` + industry.industryImage + `" />
+//                    </td>
+//                    <td class="table-action col-3">
+//                        <a onclick="getIndustryById(` + industry.industryId + `)" data-bs-toggle="modal" data-bs-target="#myModal">
+//                            <i class="fal fa-pen" style="color: #000000; margin-right: 25px;"></i>
+//                        </a>
+//                        <a onclick="deleteIndustry(` + industry.industryId + `)">
+//                            <i class="fal fa-trash" style="color: #000000;"></i>
+//                        </a>
+//                    </td>
+//                </tr>`
+//            }
+//            document.getElementById("tbody").innerHTML = html;
+//            toastr.info("Tìm thấy: " + industries.length + " kết quả.");
+//        }
+//    };
+//    xhr.open("GET", '/Industry/Search?name=' + encodeURIComponent(name), true);
+//    xhr.send();
+//}
+
+//document.getElementById("search").addEventListener("click", searchIndustries);
+//document.getElementById("name").addEventListener("input", searchIndustries);
+
+
+// Hiển thị kết quả tìm kiếm
+//function displaySearchResults(results) {
+//    var searchResultsDiv = document.getElementById('searchResults');
+
+//    // Xóa nội dung cũ
+//    searchResultsDiv.innerHTML = '';
+
+//    // Thêm kết quả mới vào div
+//    for (var i = 0; i < results.length; i++) {
+//        var result = results[i];
+//        var resultItem = document.createElement('div');
+//        resultItem.textContent = result;
+//        searchResultsDiv.appendChild(resultItem);
+//    }
+//}
